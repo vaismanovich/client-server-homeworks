b7bee17455c7d6359ac9c9d9f378f26f906a2292
Vitaliy Vaisman <vitaliyvaisman@gmail.com>
2021-06-22 20:05:05 +0400

    Initial Project

---
+++ Client-Server_Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Client-Server_Homework
+//
+//  Created by Vitaliy on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
